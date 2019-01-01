6f40c6071ed85e212746afc69bdef94fc0df4beb
Dmitry9 <karpov.dmitry80@gmail.com>
2019-01-01 23:39:13 +0200

    wip

--- test/login.js
+++ test/login.js
@@ -1,21 +1,21 @@
+const env = require('../.env');
 module.exports = async page => {
     if (process.env.NODE_ENV === 'development') {
-        const env = {
-            userName: 'fakeTestDummy9@gmail.com',
-            password: 'crushTestDummy',
+        const selectors = {
+            userName: env.userName,
+            password: env.password,
             emailInput: '#identifierId',
-            // passwordInput: 'input[type="password"]',
             passwordInput: '#password input',
             button1: '#identifierNext',
             button2: '#passwordNext',
         }
-        await page.waitForSelector(env.emailInput);
-        await page.type(env.emailInput, env.userName);
-        await page.click(env.button1);
+        await page.waitForSelector(selectors.emailInput);
+        await page.type(selectors.emailInput, selectors.userName);
+        await page.click(selectors.button1);
         await page.waitFor(3000);
-        await page.waitForSelector(env.passwordInput);
-        await page.type(env.passwordInput, env.password);
-        await page.waitForSelector(env.button2);
-        await page.click(env.button2);
+        await page.waitForSelector(selectors.passwordInput);
+        await page.type(selectors.passwordInput, selectors.password);
+        await page.waitForSelector(selectors.button2);
+        await page.click(selectors.button2);
     }
 }
